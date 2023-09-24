12d083894b0047f2b8f34497a472b9fed268bce2
CapybaraGort <112399283+CapybaraGort@users.noreply.github.com>
2023-09-24 14:50:46 +0300

    3 changes

--- Assets/Scripts/Entities/Projectiles/Mob/Sunstrike.cs
+++ Assets/Scripts/Entities/Projectiles/Mob/Sunstrike.cs
@@ -1,6 +1,7 @@
 using Assets.Scripts.Entities;
 using Assets.Scripts.Entities.Navigation.EntityType;
 using Assets.Scripts.Entities.Navigation.Util;
+using Assets.Scripts.Entities.Player;
 using Assets.Scripts.Entities.Stats.Interfaces.Stats;
 using Assets.Scripts.Entities.Templates;
 using System.Collections;
@@ -10,9 +11,10 @@ using UnityEngine;
 
 public class Sunstrike : MonoBehaviour
 {
-    [SerializeField] private int damage = 10;
     [SerializeField] private float radius;
     [SerializeField] private GameObject damageParticles;
+    private int mobDamage = 5;
+    private int playerDamage = 25;
     private ChaseMob owner;
     
     internal void Init(ChaseMob owner)
@@ -27,8 +29,8 @@ public class Sunstrike : MonoBehaviour
 
     public void BurnEverything()
     {
-        var playerEntities = NavigationUtil.GetAllEntitiesOfType(new EntityType<PlayerTag>().Any(), transform, radius);
-        var mobEntities = NavigationUtil.GetAllEntitiesOfType(new EntityType<MobTag>().Any(), transform, radius);
+        List<Entity> playerEntities = NavigationUtil.GetAllEntitiesOfType(new EntityType<PlayerTag>().Any(), transform, radius);
+        List<Entity> mobEntities = NavigationUtil.GetAllEntitiesOfType(new EntityType<MobTag>().Any(), transform, radius);
         List<Entity> entities = new List<Entity>(playerEntities);
         entities.AddRange(mobEntities);
 
@@ -36,8 +38,11 @@ public class Sunstrike : MonoBehaviour
         {
             foreach (Entity entity in entities)
             {
-                Instantiate(damageParticles, entity.transform.position, Quaternion.identity);
-                (entity as IDamageable).Damage(damage, owner);
+                Destroy(Instantiate(damageParticles, entity.transform.position, Quaternion.identity), 2);
+                if(entity is Player player)
+                    (player as IDamageable).Damage(playerDamage, owner);
+                else
+                    (entity as IDamageable).Damage(mobDamage, owner);
             }
         }
     }
