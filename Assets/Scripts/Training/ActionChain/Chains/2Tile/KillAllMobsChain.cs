c27844bd4ed66655388a8fbf423b4d6fed63f833
slidrex <108924768+slidrex@users.noreply.github.com>
2023-09-30 18:02:20 +0500

    fix minimap tutorial

--- Assets/Scripts/Training/ActionChain/Chains/2Tile/KillAllMobsChain.cs
+++ Assets/Scripts/Training/ActionChain/Chains/2Tile/KillAllMobsChain.cs
@@ -5,6 +5,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Assets.Editor;
+using Assets.Scripts.CompositeRoots;
+using Assets.Scripts.UI.Menu.MainMenu.Level;
 
 namespace Assets.Scripts.Training.ActionChain.Chains._2Tile
 {
@@ -17,6 +20,7 @@ namespace Assets.Scripts.Training.ActionChain.Chains._2Tile
 		}
 		private void OnCleaned()
 		{
+			LevelCompositeRoot.Instance.Runner.SetGameMode(GameMode.NONE);
 			StageController.Singleton.OnStageCleanedUp -= OnCleaned;
 			ConfirmChain();
 		}
