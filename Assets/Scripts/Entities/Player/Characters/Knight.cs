923cd1b6d0c1f127c7bfcbe06385fe60e8ddaffc
slidrex <108924768+slidrex@users.noreply.github.com>
2023-10-07 18:33:09 +0500

    heroes health boost

--- Assets/Scripts/Entities/Player/Characters/Knight.cs
+++ Assets/Scripts/Entities/Player/Characters/Knight.cs
@@ -10,7 +10,7 @@ namespace Assets.Scripts.Entities.Player.Characters
 {
     internal sealed class Knight : Player
     {
-        public override AttributeHolder Stats { get; } = new AttributeHolder(new MaxHealthStat(70), new SpeedStat(4), new DamageStat(5), new AttackSpeedStat(3));
+        public override AttributeHolder Stats { get; } = new AttributeHolder(new MaxHealthStat(85), new SpeedStat(4), new DamageStat(5), new AttackSpeedStat(3));
         public override void OnLevelUp()
         {
             Stats.Modify<DamageStat>(new Entities.Stats.StatAttributes.AttributeMask() { BaseValue = 0.15f });
