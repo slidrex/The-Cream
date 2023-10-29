ebb077b9906e96a46ca378474472eeb9220360c3
CapybaraGort <112399283+CapybaraGort@users.noreply.github.com>
2023-10-29 16:27:25 +0300

    фикс настроек

--- Assets/Scripts/Training/ActionChain/Chains/2Tile/EnterThePortalChain.cs
+++ Assets/Scripts/Training/ActionChain/Chains/2Tile/EnterThePortalChain.cs
@@ -1,12 +1,6 @@
 ﻿using Assets.Scripts.Entities.Player;
 using Assets.Scripts.Entities.Structures.Portal;
 using Assets.Scripts.GameProgress;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace Assets.Scripts.Training.ActionChain.Chains._2Tile
